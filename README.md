b02d982986913da9f90d64e23d376258f251ab2d
Nidhi Menon <menon@Nidhis-MacBook-Pro.local>
2018-10-20 23:55:34 -0400

    Make project public

---
+++ README.md
@@ -0,0 +1,11 @@
+# Team Messiahs
+### Team 37
+
+## Stack Overflow Data Analytics
+
+Included in this folder are the following submissions related to our project:
+1) Proposal
+2) Progress
+3) Slides
+4) Code
+
---
+++ code/Test.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import scipy as sc
+import numpy as np
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
+path = "./answers-csv.csv"
+data = pd.read_csv(path)
+#print data
+tags = []
+for i in range(0, 10):
+    tags.append(data.iloc[i]['tags'].split(","))
+tags = [item for sublist in tags for item in sublist]
+
+uniquetags = list(set(tags))
+print uniquetags
+print len(uniquetags)
+
+
+
+################################
+def bench_k_means(estimator, name, data):
+    t0 = time()
+    estimator.fit(data)
+    print('%-9s\t%.2fs\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
+          % (name, (time() - t0), estimator.inertia_,
+             metrics.homogeneity_score(labels, estimator.labels_),
+             metrics.completeness_score(labels, estimator.labels_),
+             metrics.v_measure_score(labels, estimator.labels_),
+             metrics.adjusted_rand_score(labels, estimator.labels_),
+             metrics.adjusted_mutual_info_score(labels,  estimator.labels_),
+             metrics.silhouette_score(data, estimator.labels_,
+                                      metric='euclidean',
+                                      sample_size=sample_size)))
+
+bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=10),
+              name="k-means++", data=data)
+
+bench_k_means(KMeans(init='random', n_clusters=n_digits, n_init=10),
+              name="random", data=data)
+
+# in this case the seeding of the centers is deterministic, hence we run the
+# kmeans algorithm only once with n_init=1
+pca = PCA(n_components=n_digits).fit(data)
+bench_k_means(KMeans(init=pca.components_, n_clusters=n_digits, n_init=1),
+              name="PCA-based",
+              data=data)
